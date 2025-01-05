eafa446679b16691398fba1648507cc416471d74
dorik84 <dorya2005@mail.ru>
2025-01-05 18:53:28 -0400

    public/book/clerkUserId/page complete

--- scheduler/src/app/(public)/book/[clerkUserId]/page.tsx
+++ scheduler/src/app/(public)/book/[clerkUserId]/page.tsx
@@ -1,10 +1,15 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { EventButton } from "@/components/ui/EventButton";
 import { db } from "@/drizzle/db";
+import { formatEventDescription } from "@/lib/formatters";
 import { clerkClient } from "@clerk/nextjs/server";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 export default async function BookingPage({ params: { clerkUserId } }: { params: { clerkUserId: string } }) {
   const events = await db.query.EventTable.findMany({
     where: ({ clerkUserId: userIdCol, isActive }, { eq, and }) => and(eq(userIdCol, clerkUserId), eq(isActive, true)),
-    orderBy: ({ name }, { desc }) => desc(name),
+    orderBy: ({ name }, { asc, sql }) => asc(sql`lower(${name})`),
   });
 
   if (events.length === 0) return notFound();
@@ -14,7 +19,38 @@ export default async function BookingPage({ params: { clerkUserId } }: { params:
 
   return (
     <div className="max-w-5xl mx-auto">
-      <div className="text-4xl md:text-5xl font-semibold mb-4 text-center">{fullName}</div>;
+      <div className="text-4xl md:text-5xl font-semibold mb-4 text-center">{fullName}</div>
+      <div className="text-muted-foreground mb-6 max-w-sm mx-auto text-center">Welcome to the scheduler page. Please follow the instructions to add an event to my calendar.</div>
+      <div className="grid gap-5 grid-cols-[repeat(auto-fill,minmax(300px,1fr))]">
+        {events.map((event) => (
+          <EventCard key={event.id} {...event}></EventCard>
+        ))}
+      </div>
     </div>
+  );
+}
+
+type EventCardProps = {
+  id: string;
+  name: string;
+  clerkUserId: string;
+  duration: number;
+  desc: string | null;
+};
+
+function EventCard({ id, name, clerkUserId, duration, desc }: EventCardProps) {
+  return (
+    <Card className="flex flex-col">
+      <CardHeader>
+        <CardTitle>{name}</CardTitle>
+        <CardDescription>{formatEventDescription(duration)}</CardDescription>
+      </CardHeader>
+      {desc != null && <CardContent className="opacity-50">{desc}</CardContent>}
+      <CardFooter className="flex justify-end gap-2 mt-auto">
+        <Button asChild>
+          <Link href={`/book/${clerkUserId}/${id}`}>Select</Link>
+        </Button>
+      </CardFooter>
+    </Card>
   );
 }
